5cad54c5eb84a2f324e4d1d7db07aa6168ff2328
Alan Woolley <alan@armedpineapple.co.uk>
2023-08-04 17:57:24 +0100

    Fix menu window always remaining attached even when hidden
    This was gobbling up press events silently.

--- CorsixTH/Lua/dialogs/android_menu_button.lua
+++ CorsixTH/Lua/dialogs/android_menu_button.lua
@@ -1,41 +1,41 @@
-class "UIAndroidMenuButton" (Window)
-
-local col_bg = {
-  red = 154,
-  green = 146,
-  blue = 198,
-}
+class "UIAndroidMenuButton"(Window)
 
 function UIAndroidMenuButton:UIAndroidMenuButton(ui)
-  self:Window()
+    self:Window()
 
-  local app = ui.app
-  self.app = app
-  self.ui = ui
-  self.world = app.world
-  self.on_top = true
-  self.resizable = true
-  self.draggable = false
-  self.esc_closes = false
-  self.visible = true
-  self.x = app.config.width - 100
-  self.y = 10
-  self.width = 27
-  self.height = 28
+    local app = ui.app
+    self.app = app
+    self.ui = ui
+    self.world = app.world
+    self.on_top = true
+    self.resizable = true
+    self.draggable = false
+    self.esc_closes = false
+    self.visible = true
+    self.x = app.config.width - 100
+    self.y = 10
+    self.width = 27
+    self.height = 28
+    self.menu_open = false
 
-  self.android_menu = UIAndroidMenu(self.ui)
-  self.android_menu.visible = false
-  self:addWindow(self.android_menu)
+    self.android_menu = UIAndroidMenu(self.ui)
 
-  self.panel_sprites = self.app.gfx:loadSpriteTable("Bitmap", "android", true, self.app.gfx:loadPalette("Bitmap", "android.pal"))
+    self.panel_sprites = self.app.gfx:loadSpriteTable("Bitmap", "android", true, self.app.gfx:loadPalette("Bitmap", "android.pal"))
 
-  self:addPanel(12, 65, 0, self.width, self.height):setLabel("Menu"):makeButton(0, 0, self.width, self.height, 13, self.buttonPressed):setTooltip("Open Menu")
+    self:addPanel(12, 65, 0, self.width, self.height):setLabel("Menu"):makeButton(0, 0, self.width, self.height, 13, self.buttonPressed):setTooltip("Open Menu")
 
 end
 
 function UIAndroidMenuButton:buttonPressed()
-  print "pressed menu button"
-  self.android_menu.visible = not self.android_menu.visible
+    if (not self.menu_open) then
+        print("Opening menu")
+        self:addWindow(self.android_menu)
+        self.menu_open = true
+    else
+        print("Closing menu")
+        self:removeWindow(self.android_menu)
+        self.menu_open = false
+    end
 end
 
 function UIAndroidMenuButton:onChangeResolution(width, height)
